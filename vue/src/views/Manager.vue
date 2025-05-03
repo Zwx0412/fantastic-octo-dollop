d7d72aa46ac343c8c6c05bdfb59b28b00a8a06f5
Zwx0412 <154499833+Zwx0412@users.noreply.github.com>
2025-05-04 02:56:15 +0800

    Update Manager.vue

--- vue/src/views/Manager.vue
+++ vue/src/views/Manager.vue
@@ -46,9 +46,9 @@
             </template>
             <el-menu-item index="/manager/notice" v-if="data.user.role === 'ADMIN'">系统公告</el-menu-item>
 <!--            <el-menu-item index="/manager/advertise" v-if="data.user.role === 'ADMIN'">广告信息</el-menu-item>-->
-            <el-menu-item index="/manager/industry" v-if="data.user.role === 'ADMIN'">行业信息</el-menu-item>
+<!--            <el-menu-item index="/manager/industry" v-if="data.user.role === 'ADMIN'">行业信息</el-menu-item>-->
             <el-menu-item index="/manager/position">职位信息</el-menu-item>
-            <el-menu-item index="/manager/submit">岗位投递</el-menu-item>
+<!--            <el-menu-item index="/manager/submit">岗位投递</el-menu-item>-->
           </el-sub-menu>
           <el-sub-menu index="2" v-if="data.user.role === 'ADMIN'">
             <template #title>
